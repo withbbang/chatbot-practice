f34c6d12c123c3b575bcc3b0b158d8053df3b7b3
김영선 <gim-yeongseon@gim-yeongseon-ui-MacBookPro.local>
2023-01-14 03:14:57 +0900

    CNN 합성곱 및 최대풀링

---
+++ chapter6/6-2.py
@@ -0,0 +1,22 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+
+# MNIST 데이터셋 가져오기
+_, (x_test, y_test) = mnist.load_data()
+x_test = x_test / 255.0 # 데이터 정규화
+
+# 모델 불러오기
+model = load_model('mnist_model.h5')
+model.summary()
+model.evaluate(x_test, y_test, verbose=2)
+
+# 테스트셋에서 20번째 이미지 출력
+plt.imshow(x_test[20], cmap="gray")
+plt.show()
+
+# 테스트셋의 20번째 이미지 클래스 분류
+picks = [20] # 리스트에 있는 #번째 모두 순회하며 결과 도출해줌
+predict = model.predict(x_test[picks])
+predicted = predict.argmax(axis=1)
+print("손글씨 이미지 예측값 : ", predicted)
